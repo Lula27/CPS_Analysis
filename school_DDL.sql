098670fe2de7c0d0b20f56567d2dde973902280b
Loretta Agyemang <loretta.agyemang@gmail.com>
2018-12-02 12:28:38 -0600

    cleaned and analyze high school datasets

--- school_DDL.sql
+++ school_DDL.sql
@@ -7,15 +7,28 @@
 ** Date:   11/25/2018
 ************************************************/
 
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
 -- -----------------------------------------------------
--- Create schema chi_town
+-- -----------------------------------------------------
+-- Create schema chicago 
 -- -----------------------------------------------------
 
-CREATE SCHEMA IF NOT EXISTS `chi_town` DEFAULT CHARACTER SET latin1;
-USE `chi_town`;
+CREATE SCHEMA IF NOT EXISTS `chicago` DEFAULT CHARACTER SET latin1;
+USE `chicago`;
 
 
 -- -----------------------------------------------------
 -- Table `sakila_snowflake`.`dim_actor`
 -- Dim refers to dimension table which stores data about ways data in fact talbe can be analyzed
 -- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `chicago`.`elementry`( 
+	`Zipcode` MEDIUMINT(5) ZEROFILL NOT NULL,  
+    `SchoolID` MEDIUMINT(6) ZEROFILL NOT NULL, 
+    `SchoolName` VARCHAR(45) NOT NULL, 
+    `STATE` CHAR(2) NOT NULL, 
+    `Probation` ENUM('No', 'Yes'), 
+    `ProbationYears` TINYINT(2), 
+    
